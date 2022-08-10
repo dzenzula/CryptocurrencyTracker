8980d8ae62df6362c3ab1bc43b62ab70336afab6
dzenzula <dzenzura.artjom@gmail.com>
2022-08-10 20:42:22 +0300

    Config service for coincap

---
+++ CryptocurrencyTracker.Library/Services/CoinCapService.cs
@@ -0,0 +1,64 @@
+﻿using CryptocurrencyTracker.Library.Helpers;
+using CryptocurrencyTracker.Library.DTOs;
+using Newtonsoft.Json;
+using CryptocurrencyTracker.Library.Models;
+
+namespace CryptocurrencyTracker.Library.Services
+{
+    public class CoinCapService : ICoinCapService
+    {
+        private HttpClient _httpClient;
+        public CoinCapService()
+        {
+            _httpClient = ClientHelper.GetClient(ClientHelper.CoinCap);
+        }
+
+        public async Task<List<BaseCryptoModel>> GetAllCoinsAsync()
+        {
+            var parsedCryptoList = new List<BaseCryptoModel>();
+            HttpResponseMessage response = await _httpClient.GetAsync("assets").ConfigureAwait(false);
+            var contentJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            contentJson = contentJson.Replace("null", "0");
+            Coins content = JsonConvert.DeserializeObject<Coins>(contentJson);
+
+            foreach (var coin in content.data)
+            {
+                parsedCryptoList.Add(new BaseCryptoModel
+                {
+                    Id = coin.Id,
+                    Rank = coin.Rank,
+                    Symbol = coin.Symbol,
+                    Name = coin.Name,
+                    PriceUsd = coin.PriceUsd,
+                    MarketCapUsd = coin.MarketCapUsd,
+                    Supply = coin.Supply,
+                    MaxSupply = coin.MaxSupply,
+                    ChangePercent24Hr = coin.ChangePercent24Hr,
+                    VolumeUsd24Hr = coin.VolumeUsd24Hr
+                });
+            }
+
+            return parsedCryptoList;
+        }
+
+        public async Task<List<CoinHistory>> GetCoinHistory(string id, string interval)
+        {
+            var parsedHistoryList = new List<CoinHistory>();
+            HttpResponseMessage response = await _httpClient.GetAsync($"assets/{id}/history?interval={interval}").ConfigureAwait(false);
+            var contentJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            contentJson = contentJson.Replace("null", "0");
+            CoinHistories content = JsonConvert.DeserializeObject<CoinHistories>(contentJson);
+
+            foreach (var coinHistory in content.data)
+            {
+                parsedHistoryList.Add(new CoinHistory
+                {
+                    Value = coinHistory.Value,
+                    DateTime = DateTimeOffset.FromUnixTimeMilliseconds(coinHistory.Time).DateTime
+                });
+            }
+
+            return parsedHistoryList;
+        }
+    }
+}
---
+++ CryptocurrencyTracker.Library/Services/ICoinCapService.cs
@@ -0,0 +1,11 @@
+﻿
+using CryptocurrencyTracker.Library.Models;
+
+namespace CryptocurrencyTracker.Library.Services
+{
+    public interface ICoinCapService
+    {
+        Task<List<BaseCryptoModel>> GetAllCoinsAsync();
+        Task<List<CoinHistory>> GetCoinHistory(string id, string interval);
+    }
+}
