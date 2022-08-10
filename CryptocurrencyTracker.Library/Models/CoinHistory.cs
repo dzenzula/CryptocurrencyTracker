904b314e0e9da9dd1716be86ac81e3b414a2ad05
dzenzula <dzenzura.artjom@gmail.com>
2022-08-10 20:40:43 +0300

    Added models

---
+++ CryptocurrencyTracker.Library/Models/CoinHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyTracker.Library.Models
+{
+    public class CoinHistory
+    {
+        public decimal? Value { get; set; }
+        public DateTime? DateTime { get; set; }
+    }
+}
