0204026a051369f7dbb655e817cf7e02a30ff10a
dzenzula <dzenzura.artjom@gmail.com>
2022-08-10 20:42:00 +0300

    Helper for connection

---
+++ CryptocurrencyTracker.Library/Helpers/ClientHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyTracker.Library.Helpers
+{
+    public class ClientHelper
+    {
+        public static HttpClient GetClient(string baseAddress)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            return client;
+        }
+
+        public const string CoinCap = "https://api.coincap.io/v2/";
+    }
+}
