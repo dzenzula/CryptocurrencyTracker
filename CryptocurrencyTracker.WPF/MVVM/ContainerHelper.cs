d01df93c374b9c330479433e5035bdc0feb6c894
dzenzula <dzenzura.artjom@gmail.com>
2022-08-12 15:01:41 +0300

    Added container if there will be more services

---
+++ CryptocurrencyTracker.WPF/MVVM/ContainerHelper.cs
@@ -0,0 +1,29 @@
+﻿using CryptocurrencyTracker.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+
+namespace CryptocurrencyTracker.UI.MVVM
+{
+    public static class ContainerHelper
+    {
+        //Dependency injection
+
+        private static IUnityContainer _container;
+
+        static ContainerHelper()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<ICoinCapService, CoinCapService>(new ContainerControlledLifetimeManager());
+        }
+
+        public static IUnityContainer Container
+        {
+            get { return _container; }
+        }
+    }
+}
