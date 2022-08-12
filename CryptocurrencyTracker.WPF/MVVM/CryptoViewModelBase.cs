b637cab594037bdf43f5e1bc0f866b93d50c4dc4
dzenzula <dzenzura.artjom@gmail.com>
2022-08-12 15:06:48 +0300

    Base for changed properties

---
+++ CryptocurrencyTracker.WPF/MVVM/CryptoViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyTracker.UI.MVVM
+{
+    public abstract class CryptoViewModelBase : INotifyPropertyChanged
+    {
+        public abstract void LoadAsyncData();
+        public abstract void InitializeCommands();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged == null) return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
