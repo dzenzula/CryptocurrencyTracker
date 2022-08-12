5bfe12e20c2a2a6a755aa474207995129ddb24c4
dzenzula <dzenzura.artjom@gmail.com>
2022-08-12 15:08:55 +0300

    Added search page

---
+++ CryptocurrencyTracker.WPF/Pages/SearchPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptocurrencyTracker.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for SearchPage.xaml
+    /// </summary>
+    public partial class SearchPage : Page
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is NavButton)
+            {
+                var ClickedButton = e.OriginalSource as NavButton;
+
+                NavigationService.Navigate(ClickedButton.NavUri);
+            }
+
+            return;
+        }
+    }
+}
